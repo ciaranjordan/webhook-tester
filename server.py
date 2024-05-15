b649e23322e155f358250845fb26f6d600c0a245
Ciaran Jordan (cijordan) <cijordan@cisco.com>
2024-05-15 18:55:41 +0100

    enable debug log

--- server.py
+++ server.py
@@ -9,7 +9,7 @@ app.wsgi_app = ProxyFix(
     app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
 )
 
-socketio = SocketIO(app)
+socketio = SocketIO(app, engineio_logger=True)
 
 
 EXAMPLE_WEBHOOK_STRING = 'my-webhook'
